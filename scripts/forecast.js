af59aeb3e4203547bd54647080ade638b81639b7
sumedh27 <56512410+sumedh27@users.noreply.github.com>
2023-03-09 20:56:28 +0530

    main

---
+++ scripts/forecast.js
@@ -0,0 +1,29 @@
+const key = 'R8kxttgfE7ZV9jZKCYsaJSWzMY0hl9Vf';
+
+// get weather
+
+const getWeather = async (id) => {
+
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// get city
+const getCity = async (city) => {
+
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};
+
+
